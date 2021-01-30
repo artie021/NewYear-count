9a487fc447c1b55500268652cbafcd493f6ffb4f
Ａｒｔｉｅ_ｃｈａｎ <63522257+artie021@users.noreply.github.com>
2021-01-30 17:02:54 -0300

    Add
    --> Nova funcionalidade<--
    -O webapp agora conta até o ano novo de todo ano que se passar.

--- assets/js/count.js
+++ assets/js/count.js
@@ -1,41 +1,45 @@
-let newYear = '01/01/2021'
-current = ()=>{
+    setInterval(() => {
+        const date = new Date()
+        let year = date.getFullYear()
+        let newYear = `01/01/${year + 1}`
+        let anoNovo = new Date(newYear)
+        
+        
+        
+        
+        let second = (anoNovo - date)  / 1000
     
-    const date = new Date()
-    const anoNovo = new Date(newYear)
-    let second = (anoNovo - date)  / 1000
-
-    const days = Math.floor(second / 3600 / 24)
-    const hours =  Math.floor(second / 3600) % 24
-    const minutes = Math.floor(second / 60) %60
-    const secr = second % 60
-   
-    var day =document.getElementById('day').textContent = days
-    var hour = document.getElementById('hour').textContent = hours
-    var min = document.getElementById('min').textContent = minutes
-    var sec =document.getElementById('sec').textContent = secr.toFixed([0])
-  
-}
-current()
-setInterval(current, 1000)
-
-
-
-
-
+        const days = Math.floor(second / 3600 / 24)
+        const hours =  Math.floor(second / 3600) % 24
+        const minutes = Math.floor(second / 60) %60
+        const secr = second % 60
        
-        
-       
-        
-        
+        document.getElementById('day').textContent = days
+        document.getElementById('hour').textContent = hours
+        document.getElementById('min').textContent = minutes
+        document.getElementById('sec').textContent = secr.toFixed([0])
 
-
-
-
-
-
+        if( date.toLocaleDateString() == newYear){
     
+            let vid = document.getElementById('gifF')
+            let tela = document.getElementById('tel')
+            let img =document.getElementById('ft')
+        
+            vid.style.display = 'grid'
+            tela.style.display = 'none'
+            img.style.display ='none'
+          
+           
+        }  
+        console.log(newYear) 
+    },1000)
+     
     
+
+
+
+
+
 
 
 
